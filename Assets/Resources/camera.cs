277c217fe7dcbac37f57a0902d1c47102738ebc9
just a random! <mchen8969@gmail.com>
2024-10-13 04:00:46 -0700

    a

---
+++ Assets/Resources/camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.Play();
+        transform.position = new Vector3(GameObject.Find("Player").transform.position.x, GameObject.Find("Player").transform.position.y + 1,  - 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(GameObject.Find("Player").transform.position.x, GameObject.Find("Player").transform.position.y + 1,  - 2);
+    }
+}
